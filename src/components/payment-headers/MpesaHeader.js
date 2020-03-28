ce8340db2ad8c7229c8167ed2653cd40d52ba713
mwanjajoel <joelsilverworks@gmail.com>
2020-03-29 00:57:37 +0300

    commented out redundant import

--- src/components/payment-headers/MpesaHeader.js
+++ src/components/payment-headers/MpesaHeader.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { Icon } from 'react-native-elements'
-import Header from 'react-native-rave/src/components/Header';
+// import Header from 'react-native-ravepay/src/components/Header';
 
 export default class MpesaHeader extends Component {
     constructor(props) {
